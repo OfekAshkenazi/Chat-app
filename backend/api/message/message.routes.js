6d035cb03424578f08d7cd5b45ed626126aa9cf9
ofek ashkenazi <ofeka18@gmail.com>
2024-03-24 14:48:30 +0200

    added getMessage route

--- backend/api/message/message.routes.js
+++ backend/api/message/message.routes.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { sendMessage } from "./message.controller.js"
+import { sendMessage,getMessage } from "./message.controller.js"
 import { requireAuth } from "../../middleware/requireAuth.middleware.js"
 
 
 const router = express.Router()
 
 
+router.get("/:id", requireAuth, getMessage)
 router.post("/send/:id", requireAuth, sendMessage)
 
 
