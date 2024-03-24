1241f944bb553adf7d3025b5113ebd2064dc3ea5
ofek ashkenazi <ofeka18@gmail.com>
2024-03-24 21:09:36 +0200

    added logic to gender swap on signup.

--- frontend/src/components/GenderBox.jsx
+++ frontend/src/components/GenderBox.jsx
@@ -1,23 +1,27 @@
-export default function GenderBox() {
+export default function GenderBox({ handleCheckBox, selectedGender }) {
     return (
         <section className="flex">
             <div className="form-control">
-                <label className={`label gap-2 cursor-pointer`}>
+                <label className={`label gap-2 cursor-pointer ${selectedGender === "male" && "selected"}`}>
                     <span className="label-text text-gray-300">Male</span>
                     <input
                         type="checkbox"
                         className="checkbox border-slate-900"
+                        checked={selectedGender === "male"}
+                        onChange={() => handleCheckBox("male")}
                     />
                 </label>
 
             </div>
 
             <div className="form-control">
-                <label className={`label gap-2 cursor-pointer`}>
+                <label className={`label gap-2 cursor-pointer ${selectedGender === "female" && "selected"}`}>
                     <span className="label-text text-gray-300">Female</span>
                     <input
                         type="checkbox"
                         className="checkbox border-slate-900 "
+                        checked={selectedGender === "female"}
+                        onChange={() => handleCheckBox("female")}
                     />
                 </label>
 
