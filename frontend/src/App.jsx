cad6c11b25ba23e7b21b95580888737b1a392c75
ofek ashkenazi <ofeka18@gmail.com>
2024-03-24 17:15:41 +0200

    open the cmp that i need for now and start working on the login page, added background image and glass shadow to the main div. edit it with https://tailwindcss-glassmorphism.vercel.app/

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
+import Login from "./components/Login";
+
 export default function App() {
 
   return (
-    <main className="">hello
+    <main className="p-4 h-screen flex items-center justify-center">
 
-
+      <Login />
     </main>
-
   )
 }
 
---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,5 @@
+export default function Signup() {
+    return (
+        <h2>home</h2>
+    )
+}
