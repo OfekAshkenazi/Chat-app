799d47ac04073763a09bbecdf2056c2db43ed35d
ofek ashkenazi <ofeka18@gmail.com>
2024-03-24 19:52:27 +0200

    added cmp to when no chat is open.

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,19 +1,28 @@
+import { useState } from "react";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
+import  MessageContainerNotActive  from './MessageContainerNotActive'
 
 export default function MessageContainer() {
+
+    const [chatSelected, setChatSelected] = useState(false)
+
+
     return (
         <section className="md:min-w-[450px] flex flex-col">
 
-            <header>
-                <div className="bg-slate-500 px-4 py-2 mb-2">
-                    <span className="text-gray-900 font-bold">ofekasss</span>
-                </div>
-            </header>
+            {chatSelected ?
+                <>
+                    <header>
+                        <div className="bg-slate-500 px-4 py-2 mb-2">
+                            <span className="text-gray-900 font-bold">ofekasss</span>
+                        </div>
+                    </header>
 
-            <Messages />
-
-            <MessageInput />
+                    <Messages />
+                    <MessageInput />
+                </>
+                : <MessageContainerNotActive />}
 
         </section>
     )
