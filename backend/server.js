9a9b1f60d943503596c4f86dc3f8633588bda595
ofek ashkenazi <ofeka18@gmail.com>
2024-03-24 11:03:19 +0200

    change type of script to be module and not common js. enable to simple import instad of require.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
-const express = require("express")
+import express from "express"
+import dotenv from "dotenv"
 
 const app = express()
+dotenv.config()
+
+
+const PORT = process.env.PORT || 5000
 
 app.get("/", (req,res) => {
     res.send("ofek the king you made it here")
 })
 
-app.listen(5000, () => console.log("server is running on port 5000"))
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
