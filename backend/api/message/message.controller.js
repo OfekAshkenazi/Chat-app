cccd35d1dd78c1cd0fe531a42aa0a3a236cc0bc9
ofek ashkenazi <ofeka18@gmail.com>
2024-03-24 14:48:12 +0200

    added a sent msg function and test it on postman. using promise all to do 2 async requset at the same time.

--- backend/api/message/message.controller.js
+++ backend/api/message/message.controller.js
@@ -1,7 +1,43 @@
+import Conversation from './../../models/conversation.model.js';
+import Message from './../../models/message.model.js';
+
 export async function sendMessage(req, res) {
     try {
+        const { message } = req.body
+        const { id: receiverId } = req.params
+        const senderId = req.user._id
+
+        // this will find a converstion where the participants array need to be inculde with all the array i provied { $all: [senderId, receiverId] }
+        let converstion = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId] }
+        })
+
+        if (!converstion) {
+            converstion = await Conversation.create({
+                participants: [senderId, receiverId]
+            })
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message,
+        })
+
+        if(newMessage) {
+            converstion.messages.push(newMessage._id)
+        }
+
+        await Promise.all([converstion.save(),newMessage.save()])
+
+        res.status(201).json(newMessage)
 
     } catch (error) {
-
+        console.log("Error in sendMessage controller", error.message)
+        res.status(500).json({ error: "Internal server Error" })
     }
+}
+
+export async function getMessage(req,res) {
+    
 }
