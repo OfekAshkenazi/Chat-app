e1c3834c4a22d1102f6aaba36fc004859c8a2182
ofek ashkenazi <ofeka18@gmail.com>
2024-03-24 19:59:49 +0200

    sort of end working on the ui, added routes to home cmp.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
 import Login from "./components/Login";
 import Signup from './components/Signup';
 import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
 
   return (
     <main className="p-4 h-screen flex items-center justify-center">
 
-      <Home />
+
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />}/>
+        <Route path="signup" element={<Signup />}/>
+      </Routes>
+
     </main>
   )
 }
