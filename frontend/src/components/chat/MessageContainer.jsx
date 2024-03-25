190e623f38a9dd45a42f7a43ba49e2eb62fd5c4a
ofek ashkenazi <ofeka18@gmail.com>
2024-03-25 12:20:25 +0200

    work on the state of msgContainer and create a unmount function for reset state.

--- frontend/src/components/chat/MessageContainer.jsx
+++ frontend/src/components/chat/MessageContainer.jsx
@@ -1,21 +1,28 @@
-import { useState } from "react";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
-import  MessageContainerNotActive  from './MessageContainerNotActive'
+import MessageContainerNotActive from './MessageContainerNotActive'
+import useConverstion from "../../store/useConverstion";
+import { useEffect } from "react";
 
 export default function MessageContainer() {
 
-    const [chatSelected, setChatSelected] = useState(false)
+    const { selectedConverstion, setSelectedConverstion } = useConverstion()
 
+    useEffect(() => {
+        return () => {
+            setSelectedConverstion(null)
+        }
+    }, [setSelectedConverstion])
 
     return (
         <section className="md:min-w-[450px] flex flex-col">
 
-            {chatSelected ?
+            {selectedConverstion ?
                 <>
                     <header>
                         <div className="bg-slate-500 px-4 py-2 mb-2">
-                            <span className="text-gray-900 font-bold">ofekasss</span>
+                            <span className="label-text">To: </span>
+                            <span className="text-gray-900 font-bold text-transform: capitalize">{selectedConverstion.fullName}</span>
                         </div>
                     </header>
 
