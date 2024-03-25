55c465335de2d8591f6f03c42159ad371ac2f763
ofek ashkenazi <ofeka18@gmail.com>
2024-03-25 15:37:10 +0200

    added search user logic to search bar.

--- frontend/src/components/sideBar/SearchInput.jsx
+++ frontend/src/components/sideBar/SearchInput.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import useConverstion from './../../store/useConverstion';
+import useGetConversations from './../../hooks/useGetConversations';
+import toast from "react-hot-toast";
 
 export default function SearchInput() {
+    const [search, setSerach] = useState("")
+    const { setSelectedConverstion } = useConverstion()
+    const { conversations } = useGetConversations()
+
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        if (!search) return
+        if (search.length < 3) {
+            return toast.error("Need to be at least 3 letters.")
+        }
+        const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()))
+
+        if (conversation) {
+            setSelectedConverstion(conversation)
+            setSerach('')
+        } else toast.error("No conversation was found")
+
+    }
+
     return (
-        <form className="flex item-center gap-2">
-            <input type="text" placeholder="Search..." className="input input-bordered rounded-full" />
+        <form className="flex item-center gap-2" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Search..."
+                className="input input-bordered rounded-full"
+                value={search}
+                onChange={(e) => setSerach(e.target.value)}
+            />
 
             <button type="submit" className="btn btn-circle bg-sky-500 text-white hover:bg-sky-700 ">
                 <IoSearchSharp className="w-6 h-6 outline-none" />
@@ -11,4 +41,4 @@ export default function SearchInput() {
 
         </form>
     )
-}
+} 
