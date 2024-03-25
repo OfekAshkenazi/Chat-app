e092a07e4ce86b357bc9b7c0083d66f38e8ce4d4
ofek ashkenazi <ofeka18@gmail.com>
2024-03-25 16:01:53 +0200

    install socket.io-client and build provider with usecontext.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom"
 import { AuthContextProvider } from './context/AuthContext.jsx'
+import { SocketContextProvider } from './context/SocketContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <BrowserRouter >
 
     <AuthContextProvider>
+      
+      <SocketContextProvider>
 
-      <App />
+         <App />
+
+      </SocketContextProvider>
 
     </AuthContextProvider>
 
