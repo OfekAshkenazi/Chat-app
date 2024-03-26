e11e2b4fc1753c59b36e1365a313316041dd040d
ofek ashkenazi <ofeka18@gmail.com>
2024-03-26 14:58:17 +0200

    deploy with sockets

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (loggedinUser) {
-            const socket = io(["http://localhost:5000", "https://chat-app-fzxa.onrender.com"], {
+            const socket = io("https://chat-app-fzxa.onrender.com", {
                 query: {
                     userId: loggedinUser._id
                 }
