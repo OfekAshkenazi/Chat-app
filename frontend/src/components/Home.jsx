f814c64ea47007a171b539ebf023b73aa99ff157
ofek ashkenazi <ofeka18@gmail.com>
2024-03-26 18:49:01 +0200

    some small css change at home layout

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,12 @@ import SideBar from './sideBar/SideBar';
 
 export default function Home() {
     return (
-        <section className='flex h-[640px] sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 relative'>
+        <section
+            className='flex h-[640px] sm:h-[550px] md:h-[550px] rounded-lg overflow-hidden
+         bg-gray-400 bg-clip-padding backdrop-filter
+          backdrop-blur-lg bg-opacity-0 relative
+          w-[375px] sm:min-w-[640px] md:min-w-[695px]
+          '>
             <SideBar />
             <MessageContainer />
 
