4722e5365f8ce105c05d37df74cb25e236f73e09
ofek ashkenazi <ofeka18@gmail.com>
2024-03-26 18:40:17 +0200

    added seen and image key to msg object

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -14,6 +14,13 @@ const messageSchema = new mongoose.Schema({
     message: {
         type: String,
         required: true
+    },
+    seen: {
+        type: Boolean,
+        default: false
+    },
+    image:{
+        type:String
     }
 }, { timestamps: true })
 
